ebc038a91599986dbe9377c434eaa77424e1a8d3
SeanZarzycki <seanzarz@gmail.com>
2017-01-12 01:09:48 -0500

    Range func

--- timeutil.go
+++ timeutil.go
@@ -10,6 +10,23 @@ type Observable interface {
 
 type TimeSeries []Observable
 
-
+// Range returns observables in range [start, finish].
+func (ts TimeSeries) Range(start, finish time.Time) TimeSeries {
+    var startIdx, endIdx int
+    for i := 1; i < len(ts); i++ {
+        if ts[i].After(start) || ts[i].Equal(start) { 
+            startIdx = i
+            break
+        }
+    }
+    for j := len(ts)-1; j > i; j-- {
+                if ts[j].Before(finish) || ts[j].Equal(finish) { 
+            endIdx = j
+            break
+        }
+    }
+    
+    return ts[startIdx:endIdx+1]
+}
 
 
